4dddf24c570e3c073087e68d3482751c9f908a63
usama0011 <za5232208@gmail.com>
2024-04-25 14:02:57 +0500

    Update Footer

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -172,7 +172,7 @@ function Footer() {
       </div>
       <hr className="divider" />
       <div className="footer--content--container">
-        <p className="footer--content">Made with 💖 by John</p>
+        <p className="footer--content">Made with 💖 Kamial Aqeel</p>
         <div className="footer--social--icon">
           <ul>
             <li>
